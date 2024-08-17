44ce79facf11aa0a357d562a7813e99be822212a
Nihar <155672494+Nihar05032@users.noreply.github.com>
2024-08-18 02:25:47 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.querySelector('.dropdown-btn').addEventListener('click', function() {
-    this.classList.toggle('active');
-    let dropdownContent = this.nextElementSibling;
-    if (dropdownContent.style.display === "block") {
-        dropdownContent.style.display = "none";
-    } else {
-        dropdownContent.style.display = "block";
-    }
-});
